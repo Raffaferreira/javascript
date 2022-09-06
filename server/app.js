ff9a03e331f81b429b4a6524e2be132498c18409
Rafael Ferreira <Raffaferreira@users.noreply.github.com>
2022-09-06 20:51:43 -0300

    adding morgan to logging service

--- server/app.js
+++ server/app.js
@@ -1,8 +1,7 @@
 import express from 'express'
 import path from 'path';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-
+import { logger, morgan } from 'morgan';
 import contactRouting from './routes/contact';
 import indexRouting from './routes/index'; 
 
@@ -10,6 +9,7 @@ var app = express()
 
 app.set('views', path.join(__dirname, '../public/views/'));
 app.use(logger('dev'));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
